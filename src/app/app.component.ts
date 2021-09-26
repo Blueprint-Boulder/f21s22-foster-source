7ab65cc992292d5ec2dd0b7c54e16690c29cc7de
Jett Crowson <jecr4062@colorado.edu>
2021-09-26 13:55:20 -0600

    Hopefully last test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'f21s22-foster-source';
 }
 
 function test() {
-  return "";
+  return '';
 }
