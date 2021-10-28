a7ca4a127ce9d225de931ee4ceb257140c52d43a
Jett Crowson <jecr4062@colorado.edu>
2021-10-27 21:53:38 -0600

    Fix tests

--- src/app/login/create-account-modal/create-account-modal.component.spec.ts
+++ src/app/login/create-account-modal/create-account-modal.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateAccountModalComponent } from './create-account-modal.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('CreateAccountModalComponent', () => {
   let component: CreateAccountModalComponent;
@@ -10,7 +13,7 @@ describe('CreateAccountModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ CreateAccountModalComponent ],
-      imports: [ReactiveFormsModule]
+      imports: [ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
   });
