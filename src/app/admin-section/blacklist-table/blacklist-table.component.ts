a30458f9eb50a7436c256ea7a56342fa424f34f2
Jett Crowson <jecr4062@colorado.edu>
2021-10-29 12:13:01 -0600

    Remove db service from blacklist table

--- src/app/admin-section/blacklist-table/blacklist-table.component.ts
+++ src/app/admin-section/blacklist-table/blacklist-table.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { DatabaseService } from '../../services/database-service/database.service';
-import { databaseServiceProvider } from '../../services/database-service/database.service.provider';
 import { ToastService } from '../../services/toast-service/toast.service';
 import {
   BlacklistedUser,
+  GetBlacklistedUsersRes,
   RemoveFromBlacklistRequest,
   RemoveFromBlacklistResponse,
 } from '../../models/blacklisted-user.model';
@@ -11,25 +10,27 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { formatDate } from '@angular/common';
 import { Applicant } from '../../models/applicant.model';
 import { ToastPresets } from '../../models/toast.model';
+import { BlacklistService } from '../../services/blacklist-service/blacklist.service';
+import { blacklistServiceProvider } from '../../services/blacklist-service/blacklist.service.provider';
 
 @Component({
   selector: 'app-blacklist-table',
   templateUrl: './blacklist-table.component.html',
   styleUrls: ['./blacklist-table.component.scss'],
-  providers: [databaseServiceProvider],
+  providers: [blacklistServiceProvider],
 })
 export class BlacklistTableComponent implements OnInit {
   public blacklist: BlacklistedUser[] = [];
 
   constructor(
-    private dbService: DatabaseService,
+    private blacklistService: BlacklistService,
     private toastService: ToastService
   ) {}
 
   ngOnInit(): void {
-    this.dbService.getBlacklist().subscribe(
-      (blacklistedUsers: BlacklistedUser[]) => {
-        this.blacklist = blacklistedUsers;
+    this.blacklistService.getBlacklistedUsers().subscribe(
+      (res: GetBlacklistedUsersRes) => {
+        this.blacklist = res.blacklist;
       },
       (error: HttpErrorResponse) => {
         this.toastService.show({
@@ -55,27 +56,22 @@ export class BlacklistTableComponent implements OnInit {
       phone: toRemove.phoneNumber,
     };
 
-    this.dbService.removeFromBlacklist(params).subscribe(
-      (res: RemoveFromBlacklistResponse) => {
-        if (res.error) {
-          this.toastService.show({
-            body: 'Something went wrong trying to unban the user.',
-            preset: ToastPresets.ERROR,
-          });
-        } else {
+    this.blacklistService
+      .deleteFromBlacklist(params.phone, params.email)
+      .subscribe(
+        (res: any) => {
           this.toastService.show({
             body: `Successfully unbanned user ${toRemove.firstName} ${toRemove.lastName}.`,
             preset: ToastPresets.SUCCESS,
           });
+        },
+        (error: HttpErrorResponse) => {
+          this.toastService.show({
+            body: 'Something went wrong trying to unban the user.',
+            preset: ToastPresets.ERROR,
+          });
         }
-      },
-      (error: HttpErrorResponse) => {
-        this.toastService.show({
-          body: 'Something went wrong trying to unban the user.',
-          preset: ToastPresets.ERROR,
-        });
-      }
-    );
+      );
   }
 
   public getFormattedDateForUser(index: number): string {
