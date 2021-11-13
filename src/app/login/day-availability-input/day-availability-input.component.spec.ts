0f38a4768621b271ab853478cc519c41d420e115
Jett Crowson <jettcrowson@gmail.com>
2021-11-13 14:19:28 -0700

    Fix test

--- src/app/login/day-availability-input/day-availability-input.component.spec.ts
+++ src/app/login/day-availability-input/day-availability-input.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DayAvailabilityInputComponent } from './day-availability-input.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 describe('DayAvailabilityInputComponent', () => {
   let component: DayAvailabilityInputComponent;
@@ -8,7 +9,8 @@ describe('DayAvailabilityInputComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DayAvailabilityInputComponent ]
+      declarations: [ DayAvailabilityInputComponent ],
+      imports: [ReactiveFormsModule]
     })
     .compileComponents();
   });
