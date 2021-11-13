28b9c49e832721ff502e07da0aeaec60727cd6ab
Jett Crowson <jettcrowson@gmail.com>
2021-11-13 15:20:14 -0700

    Fix tests

--- src/app/login/profile-image-upload/profile-image-upload.component.spec.ts
+++ src/app/login/profile-image-upload/profile-image-upload.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileImageUploadComponent } from './profile-image-upload.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 describe('ProfileImageUploadComponent', () => {
   let component: ProfileImageUploadComponent;
@@ -8,7 +9,11 @@ describe('ProfileImageUploadComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProfileImageUploadComponent ]
+      declarations: [ ProfileImageUploadComponent ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
   });
