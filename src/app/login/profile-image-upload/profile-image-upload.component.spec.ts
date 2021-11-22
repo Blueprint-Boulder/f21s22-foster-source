5493725f34f7ece2ffa7993936d4b37b7817926a
Jett Crowson <jecr4062@colorado.edu>
2021-11-22 00:51:02 -0700

    fix test

--- src/app/login/profile-image-upload/profile-image-upload.component.spec.ts
+++ src/app/login/profile-image-upload/profile-image-upload.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileImageUploadComponent } from './profile-image-upload.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('ProfileImageUploadComponent', () => {
   let component: ProfileImageUploadComponent;
@@ -8,7 +9,8 @@ describe('ProfileImageUploadComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProfileImageUploadComponent ]
+      declarations: [ ProfileImageUploadComponent ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
