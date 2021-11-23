c8e2d476e775a15ef576386888cccdaae8379ff3
Jett Crowson <jecr4062@colorado.edu>
2021-11-23 13:20:19 -0700

    Refactor login page auth

--- src/app/login/login-modal/login-modal.component.spec.ts
+++ src/app/login/login-modal/login-modal.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginModalComponent } from './login-modal.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('LoginModalComponent', () => {
   let component: LoginModalComponent;
@@ -11,7 +12,7 @@ describe('LoginModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LoginModalComponent ],
-      imports: [ReactiveFormsModule, HttpClientTestingModule]
+      imports: [ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
   });
