f24a01e660a0d5be4a4a5fa47506e352a88680ce
Jett Crowson <jecr4062@colorado.edu>
2021-12-23 16:36:10 -0700

    delete redundant module

--- src/app/services/address-service/address.impl.service.ts
+++ src/app/services/address-service/address.impl.service.ts
@@ -1,12 +1,11 @@
-import { AddressService } from "./address.service";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { AddressReq, SimpleAddressReq } from "../../models/adress.model";
-import { environment } from "../../../environments/environment";
+import { AddressService } from './address.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AddressReq, SimpleAddressReq } from '../../models/adress.model';
+import { environment } from '../../../environments/environment';
 
 export class AddressImplService implements AddressService {
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   updateAddress(address: SimpleAddressReq): Observable<AddressReq> {
     return this.http.put<AddressReq>(`${environment.backendHost}/api/db/account/address`, JSON.stringify(address));
