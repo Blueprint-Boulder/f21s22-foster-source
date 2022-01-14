800dc26711e75aff721acdee09d3831602d50e6b
Jett Crowson <jecr4062@colorado.edu>
2022-01-13 20:26:53 -0700

    Fix test

--- src/app/guards/pure-user/pure-user.guard.spec.ts
+++ src/app/guards/pure-user/pure-user.guard.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PureUserGuard } from './pure-user.guard';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('PureUserGuard', () => {
   let guard: PureUserGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+    });
     guard = TestBed.inject(PureUserGuard);
   });
 
