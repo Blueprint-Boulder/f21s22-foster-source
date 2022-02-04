5b520da9f4829be08a17a36ea48121053ffd02e5
Jett Crowson <jecr4062@colorado.edu>
2022-02-04 18:09:12 +0100

    Fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   backendHost: 'http://localhost:3000',
   useRealAnnouncementService: true,
   useRealAccountService: true,
