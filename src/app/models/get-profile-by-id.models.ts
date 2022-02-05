2703a0395252a4e491f4db9bb0bfba9e5e9d1f93
jettdc <58793088+jettdc@users.noreply.github.com>
2022-02-05 13:38:48 +0100

    Update main (#72)

--- src/app/models/get-profile-by-id.models.ts
+++ src/app/models/get-profile-by-id.models.ts
@@ -18,6 +18,7 @@ export interface FullProfileRes {
   photos: Photo[];
   secAccountHolder?: SecAccountHolderRes;
   account: PublicAccountFields;
+  distance?: number;
 }
 
 export interface HouseholdBackgroundRes {
