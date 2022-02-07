fa486ce5c277d54c3caef66aa821c9e0180ff3ee
jettdc <58793088+jettdc@users.noreply.github.com>
2022-02-07 18:16:29 +0100

    Update main (#74)
    * fix profile image upload button not being greyed out
    
    * Fix nav bar + small tweaks
    
    * merge main

--- src/app/services/image-service/image.mock.service.ts
+++ src/app/services/image-service/image.mock.service.ts
@@ -1,10 +1,11 @@
 import { ImageService, ImgKey } from './image.service';
 import { Observable, of } from 'rxjs';
 import { ImagePostRes } from '../../models/image.model';
+import { delay } from 'rxjs/operators';
 
 export class ImageMockService implements ImageService {
   public uploadImage(image: File): Observable<ImagePostRes> {
-    return of({ key: 'FAKE_IMAGE_KEY' });
+    return of({ key: 'FAKE_IMAGE_KEY' }).pipe(delay(1000));
   }
 
   public deleteImage(key: string): Observable<any> {
