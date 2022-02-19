42ceb9adf77269d0ffdda81cb87b183966e187c3
Jett Crowson <jecr4062@colorado.edu>
2022-02-19 15:15:50 +0100

    Add editing and deleting functionality to Topics

--- src/app/forum/forum-routing.module.ts
+++ src/app/forum/forum-routing.module.ts
@@ -2,15 +2,25 @@ import { CreateTopicComponent } from './create-topic/create-topic.component';
 import { ForumComponent } from './forum/forum.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { LoggedInGuard } from '../guards/logged-in/logged-in.guard';
+import { ModGuard } from '../guards/mod/mod.guard';
+import { EditTopicComponent } from './edit-topic/edit-topic.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ForumComponent,
+    canActivate: [LoggedInGuard],
   },
   {
     path: 'create-topic',
     component: CreateTopicComponent,
+    canActivate: [ModGuard],
+  },
+  {
+    path: 'edit-topic/:id',
+    component: EditTopicComponent,
+    canActivate: [ModGuard],
   },
 ];
 
