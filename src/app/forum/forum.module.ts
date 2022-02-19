97dd50367e28e07f943459cff54b5925652c70f5
jettdc <58793088+jettdc@users.noreply.github.com>
2022-02-19 15:24:07 +0100

    Topics endpoints integration + edit/delete (#93)
    * Current sprint (#90)
    
    * small tweaks
    
    * Add editing and deleting functionality to Topics

--- src/app/forum/forum.module.ts
+++ src/app/forum/forum.module.ts
@@ -8,9 +8,10 @@ import { TopicComponent } from './topic/topic.component';
 import { CreateTopicComponent } from './create-topic/create-topic.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { EditTopicComponent } from './edit-topic/edit-topic.component';
 
 @NgModule({
-  declarations: [ThreadReplyComponent, ForumComponent, TopicComponent, CreateTopicComponent],
+  declarations: [ThreadReplyComponent, ForumComponent, TopicComponent, CreateTopicComponent, EditTopicComponent],
   imports: [CommonModule, ForumRoutingModule, ReactiveFormsModule, RouterModule],
 })
 export class ForumModule {}
