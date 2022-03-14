24fcb042ba9c582dd66ce17e99aa2d0953465644
Jett Crowson <jecr4062@colorado.edu>
2022-03-14 15:19:20 +0100

    Add new pfp

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AccountPageComponent } from './account-page/account-page.component';
-import { ChangePasswordComponent } from './change-password/change-password.component';
-import { DeleteAccountComponent } from './delete-account/delete-account.component';
-import { UpdateAddressComponent } from './update-address/update-address.component';
-import { UpdatePhoneComponent } from './update-phone/update-phone.component';
 import { UpdateCaseWorkerComponent } from './update-case-worker/update-case-worker.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { UpdateAddressComponent } from './update-address/update-address.component';
+import { DeleteAccountComponent } from './delete-account/delete-account.component';
+import { AccountPageComponent } from './account-page/account-page.component';
+import { UpdatePhoneComponent } from './update-phone/update-phone.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
 const routes: Routes = [
   {
